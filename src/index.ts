c4db614cd80fd4e04d14d3b61bd37b78c522c35b
inde153 <inde153@naver.com>
2024-12-05 22:07:58 +0900

    feature : 컨버스 싱글톤

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-const str: string = "bye";
+class Grimpan {
+  constructor(canvas: HTMLElement | null) {
+    if (!canvas || canvas instanceof HTMLCanvasElement) {
+      throw new Error("canvas 엘리먼트를 입력하세요");
+    }
+  }
+}
+
+new Grimpan(document.querySelector("#canvas"));
